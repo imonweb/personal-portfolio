678e63e0cf0adb8e6f7ed11f7ebef748ce6f21c1
Imon <contact@imonweb.co.uk>
2023-06-16 10:59:07 +0100

    contact

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -44,11 +44,11 @@ const Banner = () => {
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
-            <h1>{`Hi I'm Imon `} <span className='wrap'>{text}</span></h1>
+            <h1>{`Hi! I'm Imon `} <span className='wrap'>{text}</span></h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
             <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
           </Col>
-          <Col xs={12} md={6} xl={7}>
+          <Col xs={12} md={6} xl={5}>
             <img src={headerImg} alt="Header Img" />
           </Col>
         </Row>
