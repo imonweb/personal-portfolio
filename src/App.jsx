4fe0439ea481f7b0ee3480b58bff3ecc5662b729
Imon <contact@imonweb.co.uk>
2023-06-17 09:25:00 +0100

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
+import { Footer } from './components/Footer';
 
 function App() {
    
@@ -15,6 +17,8 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
+      <Footer />
     </div>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,8 +4,8 @@ import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-// import 'animate.css';
-// import TrackVisibility from 'react-on-screen';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 const Projects = () => {
    const projects = [
@@ -45,7 +45,10 @@ const Projects = () => {
     <section className="project" id="project">
       <Container>
         <Row>
-          <Col>
+          <Col size={12}>
+            <TrackVisibility>
+            {({ isVisible }) =>
+            <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
             <h2>Projects</h2>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
@@ -60,7 +63,7 @@ const Projects = () => {
                   <Nav.Link eventKey="third">Tab 3</Nav.Link>
                 </Nav.Item>
               </Nav>
-              <Tab.Content>
+              <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                 <Tab.Pane eventKey="first">
                   <Row>
                     {
@@ -80,6 +83,8 @@ const Projects = () => {
                 <Tab.Pane eventKey="third">Lorem Ipsum</Tab.Pane>
               </Tab.Content>
             </Tab.Container>
+             </div>}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
